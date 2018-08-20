52709d50891738dfb1f6da77d59dd9696dfb795d
misteltein <itogahibiki@gmail.com>
2018-08-20 14:27:57 +0900

    Fix bug

--- heap.hpp
+++ heap.hpp
@@ -7,8 +7,8 @@ class heap
    T sum;
    T c;
    public:
-   heap<T>()noexcept{}
-   heap<T>(const T& val)noexcept;
+   heap<T>()noexcept{sum=0.0;c=0.0;}
+   heap<T>(const T& val)noexcept{sum=val;c=0.0;};
    heap<T>& operator+=(const T& val)noexcept;
    heap<T>& operator=(const T& val)noexcept;
    T get()const noexcept;
