a300aa7df220d45eb3efeb0cb323b8a19995b1f7
misteltein <itogahibiki@gmail.com>
2018-09-19 21:54:47 +0900

    Split Logger (hpp/cpp)

---
+++ Logger.cpp
@@ -0,0 +1,55 @@
+#include "Logger.hpp"
+
+log_info::log_info(const char* file_name, int line_num, log_level::type level)
+   : file_name(file_name), line_num(line_num), level(level)
+   {}
+
+template <class Logger>
+Logger& log_info::set_info_to(Logger& logger) const
+{
+   logger.set_log_info(*this);
+   return logger;
+}
+
+void logger_base::set_log_info(const log_info& info)
+{
+   file_name = info.file_name;
+   line_num  = info.line_num;
+   level     = info.level;
+}
+
+void stdout_logger::operator()(const char* format, ...) const
+{
+   std::va_list args;
+   char tsv[256]={'\0'};
+   va_start(args,format);
+   vsprintf(tsv,format,args);
+   va_end(args);
+   
+   //output
+   std::string name=RUN_NAME;
+   name+=".log";
+   std::ofstream ofs(name,std::ios::app);
+   ofs<< level <<" "<< file_name <<"("<< line_num << "): ";
+   ofs<<tsv;
+   ofs<<"\n";
+}
+
+std::ostream& operator<<(std::ostream& os, log_level::type level)
+{
+   switch (level) {
+      case log_level::DEBUG: os << "DEBUG"; break;
+      case log_level::INFO:  os << "INFO" ; break;
+      case log_level::WARN:  os << "WARN" ; break;
+      case log_level::ERROR: os << "ERROR"; break;
+      case log_level::FATAL: os << "FATAL"; break;
+      default: break;
+   }
+   return os;
+}
+#define DEBUG log_info(__FILE__, __LINE__, log_level::DEBUG).set_info_to(logger)
+#define INFO log_info(__FILE__, __LINE__, log_level::INFO).set_info_to(logger)
+#define WARN log_info(__FILE__, __LINE__, log_level::WARN).set_info_to(logger)
+#define ERROR log_info(__FILE__, __LINE__, log_level::ERROR).set_info_to(logger)
+#define FATAL log_info(__FILE__, __LINE__, log_level::FATAL).set_info_to(logger)
+
--- Logger.hpp
+++ Logger.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include <cstdarg>
+#include <ostream>
+#include <fstream>
 
 struct log_level
 {
@@ -13,17 +15,7 @@ struct log_level
    };
 };
 
-std::ostream& operator<<(std::ostream& os, log_level::type level)
-{
-   switch (level) {
-      case log_level::DEBUG: os << "DEBUG"; break;
-      case log_level::INFO:  os << "INFO" ; break;
-      case log_level::WARN:  os << "WARN" ; break;
-      case log_level::ERROR: os << "ERROR"; break;
-      case log_level::FATAL: os << "FATAL"; break;
-   }
-   return os;
-}
+std::ostream& operator<<(std::ostream& os, log_level::type level);
 
 class log_info;
 
@@ -45,54 +37,16 @@ friend class logger_base;
    const int   line_num;
    const       log_level::type level;
 public:
-   log_info(const char* file_name, int line_num, log_level::type level)
-   : file_name(file_name), line_num(line_num), level(level)
-   {}
-   //const member initalize: This initialization is somewhat special. 
+   log_info(const char* file_name, int line_num, log_level::type level);
 
    template <class Logger>
-   Logger& set_info_to(Logger& logger) const
-   {
-      logger.set_log_info(*this);
-      return logger;
-   }
+   Logger& set_info_to(Logger& logger) const;
 };
-
-void logger_base::set_log_info(const log_info& info)
-{
-   file_name = info.file_name;
-   line_num  = info.line_num;
-   level     = info.level;
-}
-
-//MACRO has the necessity which can access Logger
-#define DEBUG log_info(__FILE__, __LINE__, log_level::DEBUG).set_info_to(logger)
-#define INFO log_info(__FILE__, __LINE__, log_level::INFO).set_info_to(logger)
-#define WARN log_info(__FILE__, __LINE__, log_level::WARN).set_info_to(logger)
-#define ERROR log_info(__FILE__, __LINE__, log_level::ERROR).set_info_to(logger)
-#define FATAL log_info(__FILE__, __LINE__, log_level::FATAL).set_info_to(logger)
 
 struct stdout_logger : private logger_base
 {
    using logger_base::set_log_info;
-
-   void operator()(const char* format, ...) const
-   {
-      std::va_list args;
-      char tsv[256]={'\0'};
-      va_start(args,format);
-      vsprintf(tsv,format,args);
-      va_end(args);
-      
-      //output
-      std::string name=RUN_NAME;
-      name+=".log";
-      std::ofstream ofs(name,std::ios::app);
-      ofs<< level <<" "<< file_name <<"("<< line_num << "): ";
-      ofs<<tsv;
-      ofs<<"\n";
-   }
-
+   void operator()(const char* format, ...) const;
 };
 
 //NOTE:: Sample code
@@ -105,5 +59,3 @@ struct stdout_logger : private logger_base
 //}
 
 //Special thanks for bleis-tift=san
-
-stdout_logger logger;
