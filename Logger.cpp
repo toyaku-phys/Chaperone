8ef93320a183ca6685458119a0771ee2d60339d9
misteltein <itogahibiki@gmail.com>
2018-09-19 22:02:15 +0900

    Split Logger

--- Logger.cpp
+++ Logger.cpp
@@ -50,9 +50,4 @@ std::ostream& operator<<(std::ostream& os, log_level::type level)
    }
    return os;
 }
-#define DEBUG log_info(__FILE__, __LINE__, log_level::DEBUG).set_info_to(logger)
-#define INFO log_info(__FILE__, __LINE__, log_level::INFO).set_info_to(logger)
-#define WARN log_info(__FILE__, __LINE__, log_level::WARN).set_info_to(logger)
-#define ERROR log_info(__FILE__, __LINE__, log_level::ERROR).set_info_to(logger)
-#define FATAL log_info(__FILE__, __LINE__, log_level::FATAL).set_info_to(logger)
 
