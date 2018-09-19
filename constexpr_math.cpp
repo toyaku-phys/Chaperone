8dca34b4a194be1d74115d7f92f22a826669f4ef
misteltein <itogahibiki@gmail.com>
2018-09-19 23:47:24 +0900

    Fix typo

--- constexpr_math.cpp
+++ constexpr_math.cpp
@@ -20,8 +20,6 @@ constexpr T sqrt(T s)
     return x;
 }
 
-template constexpr double sqrt<double>(double);
-
 template <typename T>
 constexpr double max(const T& v1, const T& v2){return (v1<v2)?v2:v1;}
 
