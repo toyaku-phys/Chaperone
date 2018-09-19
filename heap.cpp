6d8cb726972d1aab4c689a24a447b12e53b09c84
misteltein <itogahibiki@gmail.com>
2018-09-19 22:47:39 +0900

    Split heap

---
+++ heap.cpp
@@ -0,0 +1,38 @@
+#include "heap.hpp"
+
+#pragma clang optimize off
+template <typename T>
+inline heap<T>& heap<T>::operator+=(const T& val) noexcept
+{
+   const auto y = val - c;
+   const auto t = (this->sum) + y;
+   this->c      = (t-(this->sum))-y;
+   this->sum    = t;
+   return *this;
+}
+#pragma clang optimize on
+
+template <typename T>
+inline heap<T>& heap<T>::operator=(const T& val) noexcept
+{
+   this->sum = val;
+   this->c   =0.0;
+   return *this;
+}
+
+template <typename T>
+inline T heap<T>::get() const noexcept
+{
+   return sum;   
+}
+
+//ex.
+//int main()
+//{
+//   heap<double> hoo;   
+//   heap+=1.23456789;
+//   heap+=10.2465725;
+//   heap+=3475.94859;
+//   printf("%1.15e\n",hoo.get());
+//}
+
